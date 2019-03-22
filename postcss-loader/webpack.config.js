4e79e78459777ce33c3bdefbf79c7443d7bf253a
Luis <luisdiazvenero@gmail.com>
2019-03-22 12:55:24 -0500

    no funciona el postcss

---
+++ postcss-loader/webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  //entry: path.resolve(__dirname,'index.js'),
+  entry: {
+    home: path.resolve(__dirname,'src/js/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // aqui van los loaders
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      },
+      {
+        // test: que tipo de archivo quiero reconocer
+        test: /\.css$/,
+        // use: que loader se va a encargar del archivo
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+
+      },
+      {
+        // test: que tipo de archivo quiero reconocer
+        test: /\.styl$/,
+        // use: que loader se va a encargar del archivo
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"]
+
+      },
+      {
+        // test: que tipo de archivo quiero reconocer
+        test: /\.less$/,
+        // use: que loader se va a encargar del archivo
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // aqui van los plugins
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
+}
