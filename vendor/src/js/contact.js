6c2cc5066399b317faf2fd1d6490f1600e5055ab
Luis Díaz Venero <luis@MacBook-Pro-de-Luis.local>
2019-04-11 16:02:46 -0500

    eligiendo dependencias comunes

---
+++ vendor/src/js/contact.js
@@ -0,0 +1,36 @@
+import '../css/estilos.css';
+//import {messages} from './message.js';
+import platziImg from '../images/platzi.jpg';
+import data from './teacher.json';
+import renderToDom from './render-to-dom.js';
+
+import React from 'react';
+import { render } from 'react-dom';
+import Teachers from './components/teachers.js';
+
+import '../css/main.less';
+
+
+
+
+render(<Teachers data={data} />, document.getElementById('container'));
+
+
+console.log(data);
+
+data.teachers.forEach((teacher)=>{
+  const element = document.createElement('li');
+  element.textContent = teacher.name;
+  renderToDom(element);
+})
+
+//document.write(messages.firstMessage);
+//messages.delayedMessage();
+
+const img = document.createElement('img');
+img.setAttribute('src', platziImg);
+img.setAttribute('width', 50);
+img.setAttribute('height', 50);
+document.body.append(img);
+// console.log('hola mundo desde webpack');
+console.log('hola desde webpack, en un webpack.config');
